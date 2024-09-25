a565ac8dd28e67816b8ee21992812320c21cdb0c
Krzysztof Szkudlarek <szkudlarek07@gmail.com>
2024-09-25 23:51:21 +0200

    sanctum login logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
 });
